c1fa9686fae6ba5c461ca1a75497d51667134f03
Muhammad Ihsan <55564001+ihsan946@users.noreply.github.com>
2019-09-23 15:15:49 +0700

    Add files via upload

---
+++ IF110118037Latihan13Relational/src/if110118037latihan13relational/IF110118037Latihan13Relational.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if110118037latihan13relational;
+
+/**
+ * @author LENOVO
+ * NAMA      : Muhammad Ihsan
+ * KELAS     : IF-1
+ * NIM       : 10118037
+ * Deskripsi Program   : Program ini berisi program Relational.
+ */
+public class IF110118037Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1==bil2));
+        System.out.println("a != b = " + (bil1!=bil2));
+        System.out.println("a > b  = " + (bil1 > bil2));
+        System.out.println("a < b  = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+    }
+    
+}
